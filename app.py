6be995b2d53211e783d7953b9fc66fb9f42797b3
unknown <johnwendellbalagot11111@gmail.com>
2020-08-19 20:46:47 +0800

    Modification

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, jsonify
 from model.model import Detection
+import base64
+import io
 
 detection_model = Detection()
 
@@ -7,12 +9,16 @@ app = Flask(__name__)
 
 @app.route("/predict", methods=["POST"])
 def process_image():
-    file = request.files['image']
-    file.save('im-received.jpg')
-    # Read the image via file.stream
-    pred = detection_model.predict('im-received.jpg')
-    print(file.stream)
-    print(pred)
+    #file = request.files['image']
+    #file.save('im-received.jpg')
+    #pred = detection_model.predict('im-received.jpg')
+    #print(file.stream)
+    #print(pred)
+    payload = request.form.to_dict(flat=False)
+    im_b64 = payload['image'][0]
+    im_binary = base64.b64decode(im_b64)
+    buf = io.BytesIO(im_binary)
+    print(detection_model.predict(buf))
 
     return jsonify({'msg': 'success'})
 
