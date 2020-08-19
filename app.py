2757c44302273f631905d2bf7a77fda8e916b780
unknown <johnwendellbalagot11111@gmail.com>
2020-08-19 20:26:29 +0800

    Modification

--- app.py
+++ app.py
@@ -5,12 +5,12 @@ detection_model = Detection()
 
 app = Flask(__name__)
 
-@app.route("/im_size", methods=["POST"])
+@app.route("/predict", methods=["POST"])
 def process_image():
     file = request.files['image']
     file.save('im-received.jpg')
     # Read the image via file.stream
-    pred = detection_model.predict(file.stream)
+    pred = detection_model.predict('im-received.jpg')
     print(file.stream)
     print(pred)
 
